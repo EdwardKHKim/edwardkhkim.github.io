3ef37bd3123ac2c1268e9e40db7319beb54c65a1
EdwardKHKim <kim.kyoohoon.edward@gmail.com>
2020-10-14 06:41:11 -0700

    Updates

---
+++ precache-manifest.af5ed57147d3c08afc081e00e3c5f87d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b639ee9e30197f5abefa5fd425d4a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "243be86455ec385e3066",
+    "url": "/static/css/main.4f23b5c9.chunk.css"
+  },
+  {
+    "revision": "4f9077651a6fe8b6c0f2",
+    "url": "/static/js/2.764c55a3.chunk.js"
+  },
+  {
+    "revision": "7694ed5eff56d74dfc4b6ac3fc78a12b",
+    "url": "/static/js/2.764c55a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "243be86455ec385e3066",
+    "url": "/static/js/main.69103885.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "b5fa1b553516d1cf8d03d6b4588058a0",
+    "url": "/static/media/CircularStd-Black.b5fa1b55.woff"
+  },
+  {
+    "revision": "6f4fe3ca482221e7c0d8c1baa5f31100",
+    "url": "/static/media/CircularStd-Bold.6f4fe3ca.woff"
+  },
+  {
+    "revision": "c47d7e581f2a563f8dedd4509c106fb8",
+    "url": "/static/media/CircularStd-Book.c47d7e58.woff"
+  },
+  {
+    "revision": "e47ea4c0b9151fe6cbd8e2f332113559",
+    "url": "/static/media/CircularStd-Medium.e47ea4c0.woff"
+  },
+  {
+    "revision": "53a6f8dd862e1f6b9e33e92bdca47836",
+    "url": "/static/media/carpool-app.53a6f8dd.png"
+  },
+  {
+    "revision": "5ce02d2f045c30a29f6ce81c3401bcb2",
+    "url": "/static/media/covid-tracker.5ce02d2f.png"
+  },
+  {
+    "revision": "4e9c86d338ff516f516f62e1d20478c2",
+    "url": "/static/media/linkedin-mark.4e9c86d3.png"
+  },
+  {
+    "revision": "9935f62cabb186c0996c606fbd6f907b",
+    "url": "/static/media/medium-mark.9935f62c.png"
+  },
+  {
+    "revision": "d4d01a32ad29cd63d1979c519646939b",
+    "url": "/static/media/nature-publication.d4d01a32.png"
+  },
+  {
+    "revision": "6967b87e60d81ca95e0ce8cf8822aea1",
+    "url": "/static/media/news-app.6967b87e.png"
+  },
+  {
+    "revision": "ff92f037f1bfdcce2a41a1ec867f738f",
+    "url": "/static/media/personal-website.ff92f037.png"
+  }
+]);
